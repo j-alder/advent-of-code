72dc245a34c6e7b83005b2a75922133484f81b1e
j-alder <jonathon.alder@gmail.com>
2022-11-04 23:03:13 -0400

    move instructions to md files

---
+++ 2015/6/soln.py
@@ -0,0 +1,15 @@
+def fmt_inst(inst):
+    if inst[0] == "turn":
+        return { "op": inst[1], "start": inst[2].split(','), "end": inst[4].strip('\n').split(',') }
+    elif inst[0] == "toggle":
+        return { "op": "toggle", "start": inst[1].split(','), "end": inst[3].strip('\n').split(',') }
+    else:
+        return
+
+def initial_grid():
+    return 
+
+def soln():
+    inst = [fmt_inst(x.split(' ')) for x in open("input.txt").readlines()]
+
+soln()
