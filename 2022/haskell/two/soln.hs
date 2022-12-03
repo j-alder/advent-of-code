82a6e4daa7bb0333ac70108a9ea95c3582593da9
j-alder <jonathon.alder@gmail.com>
2022-12-02 22:40:13 -0500

    2022 day 2 haskell

---
+++ 2022/haskell/two/soln.hs
@@ -0,0 +1,54 @@
+splitStr :: Char -> String -> [String]
+splitStr c str = if null n'' then [n']
+    else n' : splitStr c (tail n'')
+    where
+        n = span (/= c) str
+        n' = fst n
+        n'' = snd n
+
+playPt :: String -> Int
+playPt str = case str !! 2 of 
+    'X' -> 1
+    'Y' -> 2
+    'Z' -> 3
+    _ -> 0
+
+matchPt :: String -> Int
+matchPt str = case str of
+    "A X" -> 3
+    "A Y" -> 6
+    "B Y" -> 3
+    "B Z" -> 6
+    "C X" -> 6
+    "C Z" -> 3
+    _ -> 0
+
+outcome :: String -> Int
+outcome str = playPt str + matchPt str
+
+outcome2 :: String -> Int
+outcome2 str = case str of
+    "A X" -> outcome "A Z"
+    "A Y" -> outcome "A X"
+    "A Z" -> outcome "A Y"
+    "B X" -> outcome str
+    "B Y" -> outcome str
+    "B Z" -> outcome str
+    "C X" -> outcome "C Y"
+    "C Y" -> outcome "C Z"
+    "C Z" -> outcome "C X"
+
+soln1 :: [String] -> Int -> Int
+soln1 input total
+    | null input = total
+    | otherwise = soln1 (tail input) (outcome (head input) + total)
+
+soln2 :: [String] -> Int -> Int
+soln2 input total
+    | null input = total
+    | otherwise = soln2 (tail input) (outcome2 (head input) + total)
+
+main = do
+  input <- readFile "../../input/two.txt"
+  print (soln1 (splitStr '\n' input) 0)
+  print (soln2 (splitStr '\n' input) 0)
