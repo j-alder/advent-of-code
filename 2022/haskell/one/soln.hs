ef4a43ae98c46d6a2a472124c30000f12810248a
j-alder <jonathon.alder@gmail.com>
2022-12-02 19:50:19 -0500

    2022 haskell day 1

--- 2022/haskell/one/soln.hs
+++ 2022/haskell/one/soln.hs
@@ -6,8 +6,41 @@ splitStr c str = if null n'' then [n']
         n' = fst n
         n'' = snd n
 
-soln1 :: [Int] -> Int
+allToInt :: [String] -> [Int]
+allToInt strArr 
+  | null strArr = []
+  | head strArr == "" = 0 : allToInt (tail strArr)
+  | otherwise = (read (head strArr) :: Int) : allToInt (tail strArr)
+
+soln1 :: [Int] -> Int -> Int -> Int
+soln1 input currTotal highest 
+  | null input = highest
+  | head input == 0 = soln1 (tail input) 0 (max currTotal highest)
+  | otherwise = soln1 (tail input) (currTotal + head input) highest
+
+first :: (Int, Int, Int) -> Int
+first (x, _, _) = x
+
+second :: (Int, Int, Int) -> Int
+second (_, y, _) = y
+
+third :: (Int, Int, Int) -> Int
+third (_, _, z) = z
+
+newTrip :: Int -> (Int, Int, Int) -> (Int, Int, Int)
+newTrip comp trip 
+  | comp >= first trip = (comp, first trip, second trip)
+  | comp >= second trip = (first trip, comp, second trip)
+  | comp >= third trip = (first trip, second trip, comp)
+  | otherwise = trip
+
+soln2 :: [Int] -> Int -> (Int, Int, Int) -> Int
+soln2 input currTotal topThree
+  | null input = first topThree + second topThree + third topThree
+  | head input == 0 = soln2 (tail input) 0 (newTrip currTotal topThree)
+  | otherwise = soln2 (tail input) (currTotal + head input) topThree
 
 main = do
   input <- readFile "../../input/one.txt"
-  print (splitStr '\n' input)
+  print(soln1 (allToInt (splitStr '\n' input)) 0 0)
+  print(soln2 (allToInt (splitStr '\n' input)) 0 (0, 0, 0))
