39498035b1572d7817f1fea8766e7c25c88bda06
j-alder <jonathon.alder@gmail.com>
2022-12-16 21:31:09 -0500

    2022 kotlin day 12 complete

--- 2022/kotlin/day12/soln2212.kt
+++ 2022/kotlin/day12/soln2212.kt
@@ -2,77 +2,91 @@ package day12
 
 import util.Coord
 import util.inputToList
+import java.util.PriorityQueue
 
-typealias Grid = List<String>
+typealias Grid = List<List<Int>>
 
 data class PathNode(
     val coord: Coord,
-    val parent: PathNode?
-)
+    val steps: Int,
+) : Comparable<PathNode> {
+    override fun compareTo(other: PathNode) =
+        steps.compareTo(other.steps)
+}
 
-/** All possible heights in the grid */
-val heights = ('a'..'z').toList()
-
-/** Whether [m] is attainable from the current sea level [n] */
-fun isReachable(n: Char?, m: Char?): Boolean = try {
-    n == 'z' && m == 'E' ||
-    heights.indexOf(n).let { heights[it] == m || heights[it + 1] == m }
-} catch (e: Exception) { false }
-
-/**
- * Retrieve the Char at this Grid's x/y coordinate
- * specified in [c], otherwise null
- */
-fun Grid.getValueAt(c: Coord) = try {
-    this[c.first][c.second].let { if (it == 'S') 'a' else it }
-} catch (e: Exception) { null }
-
-/**
- * Return [c] if it is within the bounds of the
- * grid, otherwise null
- */
-fun Grid.getCoord(c: Coord): Coord? = try {
-    this[c.first][c.second]
-    c
-} catch (e: Exception) { null }
-
-/** Find all adjacent coordinates to [c] that can be moved to */
-fun Grid.adjacentTo(c: Coord): List<Coord> =
-    listOfNotNull(
-        getCoord(Coord(c.first, c.second - 1)),
-        getCoord(Coord(c.first, c.second + 1)),
-        getCoord(Coord(c.first - 1, c.second)),
-        getCoord(Coord(c.first + 1, c.second))
-    ).filter {
-        isReachable(getValueAt(c), getValueAt(it))
+val heights = ('a'..'z')
+    .mapIndexed { idx, c -> c to idx }
+    .toMap()
+    .toMutableMap()
+    .apply {
+        this['E'] = 26
+        this['S'] = 0
     }
 
-fun List<Coord>.undiscovered(p: Set<Coord>) = filter { !p.contains(it) }
+fun Grid.getCoord(x: Int, y: Int): Coord? =
+    try {
+        this[x][y]
+        Coord(x, y)
+    } catch (e: Exception) {
+        null
+    }
+
+fun Grid.adjacentTo(c: Coord) =
+    listOfNotNull(
+        getCoord(c.first, c.second + 1),
+        getCoord(c.first, c.second - 1),
+        getCoord(c.first + 1, c.second),
+        getCoord(c.first - 1, c.second)
+    )
+
+fun List<Coord>.reachable(from: Coord, g: Grid): List<Coord> =
+    filter { to -> g[to.first][to.second] - g[from.first][from.second] <= 1 }
+
+fun List<Coord>.undiscovered(visited: Map<Coord, Boolean>) =
+    filter { c -> visited[c] == false }
+
+fun Grid.shortestPath(startingCoord: Coord, visited: MutableMap<Coord, Boolean>): Int {
+    visited.keys.forEach { visited[it] = false }
+    val queue = PriorityQueue<PathNode>()
+    queue.add(PathNode(startingCoord, 0))
+    var curr: PathNode?
+    while (queue.isNotEmpty()) {
+        curr = queue.poll()
+        if (this[curr.coord.first][curr.coord.second] == 26) {
+            return curr.steps
+        }
+        adjacentTo(curr.coord)
+            .reachable(curr.coord, this)
+            .undiscovered(visited)
+            .forEach { c ->
+                visited[c] = true
+                queue.add(PathNode(c, curr.steps + 1))
+            }
+    }
+    return Int.MAX_VALUE
+}
+
+fun Grid.shortestPathFrom(
+    startingCoords: List<Coord>,
+    visited: MutableMap<Coord, Boolean>
+): Int = startingCoords.minOf { shortestPath(it, visited) }
 
 fun main() {
-    val grid = inputToList("/2022/input/twelve.txt")
-    val startingCoord = grid.indexOfFirst { str ->
-        str.contains('S')
-    }.let { x ->
-        Coord(x, grid[x].indexOfFirst { it == 'S' })
-    }
-    // Part 1 - BFS approach
-    val p = mutableSetOf(startingCoord)
-    val q = ArrayDeque<PathNode>()
-    q.addLast(PathNode(startingCoord, null))
-    var curr: PathNode? = null
-    while (q.isNotEmpty()) {
-        curr = q.removeFirst()
-        if (grid.getValueAt(curr.coord) == 'E') break
-        grid.adjacentTo(curr.coord).undiscovered(p).forEach {
-            p.add(it)
-            q.addLast(PathNode(it, curr))
+    var startingCoord: Coord? = null
+    val startingCoords = mutableListOf<Coord>()
+    val visited = mutableMapOf<Coord, Boolean>()
+    val grid: Grid = inputToList("/2022/input/twelve.txt")
+        .mapIndexed { idx, ln ->
+            ln.mapIndexed { idy, c ->
+                if (c == 'S') startingCoord = Coord(idx, idy)
+                if (c == 'S' || c == 'a') startingCoords.add(Coord(idx, idy))
+                visited[Coord(idx, idy)] = false
+                heights[c]!!
+            }
         }
-    }
-    var count = 0
-    while (curr?.parent != null) {
-        count++
-        curr = curr.parent
-    }
-    println("Answer 1: $count")
+    println(startingCoords)
+    val answer1 = grid.shortestPath(startingCoord!!, visited)
+    val answer2 = grid.shortestPathFrom(startingCoords, visited)
+    println("Answer 1: $answer1")
+    println("Answer 2: $answer2")
 }
