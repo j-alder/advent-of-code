03cbb8dd714e76c82f1cdc95c21108850b7f3035
Jonathon Alder <jonathon.alder@amenitieshealth.com>
2023-11-30 17:09:10 -0500

    2020.5.2.js

--- js/y2020/d05.js
+++ js/y2020/d05.js
@@ -1,26 +1,40 @@
 const { fmtSoln } = require('../util.js');
 
+function calcSeatId(boardingPass) {
+  if (boardingPass.length !== 10) return -1;
+  let [minRow, maxRow] = [0, 127];
+  let [minCol, maxCol] = [0, 7];
+  boardingPass.split('').forEach((char, i) => {
+    if (i <= 6) {
+      if (char === 'F') maxRow = Math.floor((minRow + maxRow) / 2);
+      else minRow = Math.ceil((minRow + maxRow) / 2);
+    } else {
+      if (char === 'L') maxCol = Math.floor((minCol + maxCol) / 2);
+      else minCol = Math.ceil((minCol + maxCol) / 2);
+    }
+  });
+  return minRow * 8 + minCol;
+}
+
 function partOne(input) {
   let seatId = -1;
   input.forEach(boardingPass => {
-    let [minRow, maxRow] = [0, 127];
-    let [minCol, maxCol] = [0, 7];
-    boardingPass.split('').forEach((char, i) => {
-      if (i <= 6) {
-        if (char === 'F') maxRow = Math.floor((minRow + maxRow) / 2);
-        else minRow = Math.ceil((minRow + maxRow) / 2);
-      } else {
-        if (char === 'L') maxCol = Math.floor((minCol + maxCol) / 2);
-        else minCol = Math.ceil((minCol + maxCol) / 2);
-      }
-    });
-    const newSeatId = minRow * 8 + minCol;
-    seatId = Math.max(seatId, newSeatId);
+    seatId = Math.max(seatId, calcSeatId(boardingPass));
   });
   return seatId;
 }
 
 function partTwo(input) {
+  const seats = input
+    .reduce((result, boardingPass) => {
+        const seatId = calcSeatId(boardingPass);
+        if (seatId > -1) result.push(calcSeatId(boardingPass));
+        return result;
+      }, [])
+    .sort((a, b) => a - b);
+  for (let i = 0; i < seats.length - 1; i++) {
+    if (seats[i+1] - seats[i] > 1) return seats[i] + 1;
+  }
 }
 
 function soln(rawInput) {
@@ -91,4 +105,18 @@ Here are some other boarding passes:
 
 As a sanity check, look through your list of boarding passes. What is 
 the highest seat ID on a boarding pass?
+
+--- Part Two ---
+
+Ding! The "fasten seat belt" signs have turned on. Time to find your seat.
+
+It's a completely full flight, so your seat should be the only missing 
+boarding pass in your list. However, there's a catch: some of the seats 
+at the very front and back of the plane don't exist on this aircraft, so 
+they'll be missing from your list as well.
+
+Your seat wasn't at the very front or back, though; the seats with IDs +1 
+and -1 from yours will be in your list.
+
+What is the ID of your seat?
 */
