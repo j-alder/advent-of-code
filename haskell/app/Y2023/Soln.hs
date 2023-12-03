7eaee34d980786a5fd17a01886dd2284e4f1ce7c
j-alder <jonathon.alder@gmail.com>
2023-12-03 14:31:07 -0500

    2023.3.js complete

--- haskell/app/Y2023/Soln.hs
+++ haskell/app/Y2023/Soln.hs
@@ -1,7 +1,9 @@
 module Y2023.Soln where
 import Y2023.D01
+import Y2023.D02
 
 ansForDay :: String -> String -> (String, String)
 ansForDay day input
   | day == "1" = Y2023.D01.soln input
+  | day == "2" = Y2023.D02.soln input
   | otherwise = ("incomplete", "incomplete")
