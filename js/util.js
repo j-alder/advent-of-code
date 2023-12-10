de008ba8a49d1b1be50cd380d560d86cd0e348b5
j-alder <jonathon.alder@gmail.com>
2023-12-10 15:24:03 -0500

    2023.10.js part 1 complete

--- js/util.js
+++ js/util.js
@@ -1,3 +1,12 @@
+function coordsOf(val, matrix) {
+  let x = 0;
+  while (x < matrix.length) {
+    const y = matrix[x].indexOf(val);
+    if (y > -1) return [x, y];
+    x++;
+  }
+}
+
 function withRuntime(cb) {
   const start = performance.now();
   return [cb(), performance.now() - start];
@@ -12,6 +21,25 @@ function fmtAnsWithRuntime(cb1, cb2) {
   part two: ${ansTwo ?? 'incomplete'}${ansTwo ? `\n  runtime (ms): ${rtTwo.toFixed(4)}` : ''}`);
 }
 
+/**
+ * Return all neighbors of a given coordinate in a matrix
+ * @param {*} x index of outer array
+ * @param {*} y index of inner array
+ * @param {*} matrix 2d array
+ * @returns An object with each neighbor as a key and its coordinates as a value
+ */
+const getNeighbors = (x, y, matrix) =>
+  ({
+    nw: matrix[x - 1] && [matrix[x - 1][y - 1], x - 1, y - 1],
+    n: matrix[x - 1] && [matrix[x - 1][y], x - 1, y],
+    ne: matrix[x - 1] && [matrix[x - 1][y + 1], x - 1, y + 1],
+    w: matrix[x] && [matrix[x][y - 1], x, y - 1],
+    e: matrix[x] && [matrix[x][y + 1], x, y + 1],
+    sw: matrix[x + 1] && [matrix[x + 1][y - 1], x + 1, y - 1],
+    s: matrix[x + 1] && [matrix[x + 1][y], x + 1, y],
+    se: matrix[x + 1] && [matrix[x + 1][y + 1], x + 1, y + 1],
+  });
+
 function readInput(year, day, test) {
   const fs = require('node:fs');
   const file = fs.readFileSync(`../input/y${year}/d${day < 10 ? `0${day}` : day}${test ? '_test' : ''}.txt`, 'utf8');
@@ -21,7 +49,9 @@ function readInput(year, day, test) {
 const sum = (arr) => arr.reduce((s, n) => s + n, 0);
 
 module.exports = {
+  coordsOf,
   fmtAnsWithRuntime,
+  getNeighbors,
   readInput,
   sum,
 };
--- js/y2023/d10.js
+++ js/y2023/d10.js
@@ -1,14 +1,115 @@
-const { fmtAnsWithRuntime } = require('../util.js');
+const { fmtAnsWithRuntime, getNeighbors, coordsOf } = require('../util.js');
 
-function partOne(input) {
+class Node {
+  constructor(x, y, type) {
+    this.x = x;
+    this.y = y;
+    this.type = type;
+  }
+}
+
+const validMoves = {
+  S: {
+    w: ['F', 'L', '-'],
+    e: ['J', '7', '-'],
+    n: ['|', 'F', '7'],
+    s: ['|', 'J', 'L'],
+  },
+  '|': {
+    w: [],
+    e: [],
+    n: ['|', 'F', '7', 'S'],
+    s: ['|', 'J', 'L', 'S'],
+  },
+  '-': {
+    w: ['-', 'F', 'L', 'S'],
+    e: ['-', 'J', '7', 'S'],
+    n: [],
+    s: [],
+  },
+  F: {
+    w: [],
+    e: ['-', 'J', '7', 'S'],
+    n: [],
+    s: ['|', 'J', 'L', 'S'],
+  },
+  L: {
+    w: [],
+    e: ['J', '7', '-', 'S'],
+    n: ['|', 'F', '7', 'S'],
+    s: [],
+  },
+  J: {
+    w: ['-', 'L', 'F', 'S'],
+    e: [],
+    n: ['|', 'F', '7', 'S'],
+    s: [],
+  },
+  7: {
+    w: ['-', 'L', 'F', 'S'],
+    e: [],
+    n: [],
+    s: ['|', 'J', 'L', 'S'],
+  },
+  '.': {
+    w: [],
+    e: [],
+    n: [],
+    s: [],
+  },
+};
+
+function isValidPipe(targetPipe, currPipe, dir, targetX, targetY, path) {
+  if (!targetPipe) return false;
+  if (!validMoves[currPipe][dir].includes(targetPipe)) return false;
+  const prevNode = path[path.length - 2];
+  if (!prevNode) return true;
+  return !(prevNode.x === targetX && prevNode.y === targetY);
+}
+
+const getValidPipe = (node, matrix, path) => 
+  Object.entries(getNeighbors(node.x, node.y, matrix))
+    .filter(([k, _]) => k === 'n' || k === 's' || k === 'e' || k === 'w')
+    .find(([dir, val]) => isValidPipe(val[0], node.type, dir, val[1], val[2], path));
+
+const getNextNode = (node, matrix, path) => {
+  const validPipe = getValidPipe(node, matrix, path);
+  if (!validPipe) return null;
+  return new Node(validPipe[1][1], validPipe[1][2], validPipe[1][0]);
+};
+
+const getValidPipes = (node, matrix, path) =>
+  Object.entries(getNeighbors(node.x, node.y, matrix))
+    .filter(([k, _]) => k === 'n' || k === 's' || k === 'e' || k === 'w')
+    .reduce((result, [dir, val]) => {
+      if (val && isValidPipe(val[0], node.type, dir, val[1], val[2], path)) {
+        result.push(new Node(val[1], val[2], val[0]));
+      }
+      return result;
+    }, []);
+
+function partOne(root, matrix) {
+  let steps = 0;
+  let curr = root;
+  let path = [curr];
+  let neighbor = getNextNode(curr, matrix, path);
+  while (neighbor.type !== 'S') {
+    curr = neighbor;
+    path.push(curr);
+    steps++;
+    neighbor = getNextNode(curr, matrix, path);
+  }
+  return Math.ceil(steps / 2);
 }
 
 function partTwo(input) {
 }
 
 function soln(rawInput) {
-  const input = rawInput.split('\n');
-  fmtAnsWithRuntime(() => partOne(input), () => partTwo(input));
+  const matrix = rawInput.split('\n').map(ln => ln.split(''));
+  const startingCoords = coordsOf('S', matrix);
+  const root = new Node(startingCoords[0], startingCoords[1], 'S');
+  fmtAnsWithRuntime(() => partOne(root, matrix), () => partTwo(matrix));
 }
 
 module.exports = { soln };
@@ -143,5 +244,115 @@ Here are the distances for each tile on that loop:
 
 Find the single giant loop starting at S. How many steps along the loop does it take 
 to get from the starting position to the point farthest from the starting position?
+
+--- Part Two ---
+
+You quickly reach the farthest point of the loop, but the animal never emerges. 
+Maybe its nest is within the area enclosed by the loop?
+
+To determine whether it's even worth taking the time to search for such a nest, you 
+should calculate how many tiles are contained within the loop. For example:
+
+...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........
+
+The above loop encloses merely four tiles - the two pairs of . in the southwest and 
+southeast (marked I below). The middle . tiles (marked O below) are not in the loop. 
+Here is the same loop again with those regions marked:
+
+...........
+.S-------7.
+.|F-----7|.
+.||OOOOO||.
+.||OOOOO||.
+.|L-7OF-J|.
+.|II|O|II|.
+.L--JOL--J.
+.....O.....
+
+In fact, there doesn't even need to be a full tile path to the outside for tiles to 
+count as outside the loop - squeezing between pipes is also allowed! Here, I is still 
+within the loop and O is still outside the loop:
+
+..........
+.S------7.
+.|F----7|.
+.||OOOO||.
+.||OOOO||.
+.|L-7F-J|.
+.|II||II|.
+.L--JL--J.
+..........
+
+In both of the above examples, 4 tiles are enclosed by the loop.
+
+Here's a larger example:
+
+.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...
+
+The above sketch has many random bits of ground, some of which are in the loop (I) 
+and some of which are outside it (O):
+
+OF----7F7F7F7F-7OOOO
+O|F--7||||||||FJOOOO
+O||OFJ||||||||L7OOOO
+FJL7L7LJLJ||LJIL-7OO
+L--JOL7IIILJS7F-7L7O
+OOOOF-JIIF7FJ|L7L7L7
+OOOOL7IF7||L7|IL7L7|
+OOOOO|FJLJ|FJ|F7|OLJ
+OOOOFJL-7O||O||||OOO
+OOOOL---JOLJOLJLJOOO
+
+In this larger example, 8 tiles are enclosed by the loop.
+
+Any tile that isn't part of the main loop can count as being enclosed by the loop. 
+Here's another example with many bits of junk pipe lying around that aren't connected 
+to the main loop at all:
+
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+
+Here are just the tiles that are enclosed by the loop marked with I:
+
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJIF7FJ-
+L---JF-JLJIIIIFJLJJ7
+|F|F-JF---7IIIL7L|7|
+|FFJF7L7F-JF7IIL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+
+In this last example, 10 tiles are enclosed by the loop.
+
+Figure out whether you have time to search for the nest by calculating the area within 
+the loop. How many tiles are enclosed by the loop?
 
 */
