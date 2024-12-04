62b5375bd4328ae9180749f552f9a8268a585102
Jonathon Alder <jonathon.alder@gmail.com>
2024-12-04 09:29:11 -0500

    2024.3.js brute force

---
+++ js/y2024/d04.js
@@ -0,0 +1,211 @@
+const { fmtAnsWithRuntime } = require('../util.js');
+
+function partOne(input) {
+  // find lateral spellings - 'XMAS' 'SAMX'
+  // find horizontal spellings
+  // find vertical spellings
+  // find left diagonal spellings
+  // find right diagonal spellings
+
+  return input.reduce((count, line, lineIdx, lines) => {
+    let localCount = 0;
+    for (let charIdx = 0; charIdx < line.length; charIdx++) {
+      if (line[charIdx] == 'X') {
+        // horiz
+        if (line[charIdx + 1] == 'M' && line[charIdx + 2] == 'A' && line[charIdx + 3] == 'S') {
+          // console.log(lineIdx, charIdx);
+          localCount++;
+        }
+        // vert
+        if (lines[lineIdx + 1]?.[charIdx] == 'M' && lines[lineIdx + 2]?.[charIdx] == 'A' && lines[lineIdx + 3]?.[charIdx] == 'S') {
+          // console.log(lineIdx, charIdx);
+          localCount++;
+        }
+        // diag
+        if (lines[lineIdx + 1]?.[charIdx + 1] == 'M' && lines[lineIdx + 2]?.[charIdx + 2] == 'A' && lines[lineIdx + 3]?.[charIdx + 3] == 'S') {
+          // console.log(lineIdx, charIdx);
+          localCount++;
+        }
+        // diag
+        if (lines[lineIdx + 1]?.[charIdx - 1] == 'M' && lines[lineIdx + 2]?.[charIdx - 2] == 'A' && lines[lineIdx + 3]?.[charIdx - 3] == 'S') {
+          // console.log(lineIdx, charIdx);
+          localCount++;
+        }
+      } 
+      if (line[charIdx] == 'S') {
+        // horiz
+        if (line[charIdx + 1] == 'A' && line[charIdx + 2] == 'M' && line[charIdx + 3] == 'X') {
+          // console.log(lineIdx, charIdx);
+          localCount++;
+        }
+        // vert
+        if (lines[lineIdx + 1]?.[charIdx] == 'A' && lines[lineIdx + 2]?.[charIdx] == 'M' && lines[lineIdx + 3]?.[charIdx] == 'X') {
+          // console.log(lineIdx, charIdx);
+          localCount++;
+        }
+        // diag
+        if (lines[lineIdx + 1]?.[charIdx + 1] == 'A' && lines[lineIdx + 2]?.[charIdx + 2] == 'M' && lines[lineIdx + 3]?.[charIdx + 3] == 'X') {
+          // console.log(lineIdx, charIdx);
+          localCount++;
+        }
+        if (lines[lineIdx + 1]?.[charIdx - 1] == 'A' && lines[lineIdx + 2]?.[charIdx - 2] == 'M' && lines[lineIdx + 3]?.[charIdx - 3] == 'X') {
+          // console.log(lineIdx, charIdx);
+          localCount++;
+        }
+      }
+    }
+    return count + localCount;
+  }, 0);
+  /*
+  keep "visited" map
+  for each line:
+    for each letter:
+      if 'X':
+        if next e/s/se == 'M' ...
+      if 'S':
+        if next e/s/se == 'A' ...
+  */
+}
+
+function partTwo(input) {
+  return input.reduce((count, line, lineIdx, lines) => {
+    let localCount = 0;
+    for (let charIdx = 0; charIdx < line.length; charIdx++) {
+      if (line[charIdx] == 'A') {
+        /*
+        M S
+         A
+        M S
+        */
+        if (
+          lines[lineIdx - 1]?.[charIdx - 1] == 'M' && lines[lineIdx - 1]?.[charIdx + 1] == 'S' && 
+          lines[lineIdx + 1]?.[charIdx - 1] == 'M' && lines[lineIdx + 1]?.[charIdx + 1] == 'S') {
+          // console.log(lineIdx, charIdx);
+          localCount++;
+        }
+        /*
+        M M
+         A
+        S S
+        */
+        if (
+          lines[lineIdx - 1]?.[charIdx - 1] == 'M' && lines[lineIdx - 1]?.[charIdx + 1] == 'M' && 
+          lines[lineIdx + 1]?.[charIdx - 1] == 'S' && lines[lineIdx + 1]?.[charIdx + 1] == 'S') {
+          // console.log(lineIdx, charIdx);
+          localCount++;
+        }
+        /*
+        S M
+         A
+        S M
+        */
+        if (
+          lines[lineIdx - 1]?.[charIdx - 1] == 'S' && lines[lineIdx - 1]?.[charIdx + 1] == 'M' && 
+          lines[lineIdx + 1]?.[charIdx - 1] == 'S' && lines[lineIdx + 1]?.[charIdx + 1] == 'M') {
+          // console.log(lineIdx, charIdx);
+          localCount++;
+        }
+        /*
+        S S
+         A
+        M M
+        */
+        if (
+          lines[lineIdx - 1]?.[charIdx - 1] == 'S' && lines[lineIdx - 1]?.[charIdx + 1] == 'S' && 
+          lines[lineIdx + 1]?.[charIdx - 1] == 'M' && lines[lineIdx + 1]?.[charIdx + 1] == 'M') {
+          // console.log(lineIdx, charIdx);
+          localCount++;
+        }
+      } 
+    }
+    return count + localCount;
+  }, 0);
+}
+
+function soln(rawInput) {
+  const input = rawInput.split('\n').map(ln => ln.split(''));
+  fmtAnsWithRuntime(() => partOne(input), () => partTwo(input));
+}
+
+module.exports = { soln };
+
+/*
+--- Day 4: Ceres Search ---
+"Looks like the Chief's not here. Next!" One of The Historians pulls out a device 
+and pushes the only button on it. After a brief flash, you recognize the interior 
+of the Ceres monitoring station!
+
+As the search for the Chief continues, a small Elf who lives on the station tugs 
+on your shirt; she'd like to know if you could help her with her word search 
+(your puzzle input). She only has to find one word: XMAS.
+
+This word search allows words to be horizontal, vertical, diagonal, written 
+backwards, or even overlapping other words. It's a little unusual, though, as you 
+don't merely need to find one instance of XMAS - you need to find all of them. 
+Here are a few ways XMAS might appear, where irrelevant characters have been 
+replaced with .:
+
+
+..X...
+.SAMX.
+.A..A.
+XMAS.S
+.X....
+The actual word search will be full of letters instead. For example:
+
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+In this word search, XMAS occurs a total of 18 times; here's the same word search 
+again, but where letters not involved in any XMAS have been replaced with .:
+
+....XXMAS.
+.SAMXMS...
+...S..A...
+..A.A.MS.X
+XMASAMX.MM
+X.....XA.A
+S.S.S.S.SS
+.A.A.A.A.A
+..M.M.M.MM
+.X.X.XMASX
+Take a look at the little Elf's word search. How many times does XMAS appear?
+
+--- Part Two ---
+The Elf looks quizzically at you. Did you misunderstand the assignment?
+
+Looking for the instructions, you flip over the word search to find that this 
+isn't actually an XMAS puzzle; it's an X-MAS puzzle in which you're supposed to 
+find two MAS in the shape of an X. One way to achieve that is like this:
+
+M.S
+.A.
+M.S
+Irrelevant characters have again been replaced with . in the above diagram. Within 
+the X, each MAS can be written forwards or backwards.
+
+Here's the same example from before, but this time all of the X-MASes have been 
+kept instead:
+
+.M.S......
+..A..MSMS.
+.M.S.MAA..
+..A.ASMSM.
+.M.S.M....
+..........
+S.S.S.S.S.
+.A.A.A.A..
+M.M.M.M.M.
+..........
+In this example, an X-MAS appears 9 times.
+
+Flip the word search from the instructions back over to the word search side and 
+try again. How many times does an X-MAS appear?
+*/
