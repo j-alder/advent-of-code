d67d9e790dbf07138c3da4bdf6fd2a88acc16bb8
Jonathon Alder <jonathon.alder@gmail.com>
2024-12-07 07:23:13 -0500

    2024.7.js complete

--- js/y2024/d07.js
+++ js/y2024/d07.js
@@ -1,23 +1,65 @@
-const { fmtAnsWithRuntime } = require('../util.js');
+const { fmtAnsWithRuntime } = require("../util.js");
 
-const operators = ["*", "+"];
+function permutations(permutationLength, alphabet) {
+  const result = [];
 
-function operatorPermutations(nOperands) {
+  function generate(current = "") {
+    if (current.length === permutationLength) {
+      result.push(current);
+      return;
+    }
 
+    for (const op of alphabet) {
+      generate(current + op);
+    }
+  }
+
+  generate();
+  return result;
+}
+
+function calc(operands, operators) {
+  let n = operands[0];
+  for (let i = 1; i < operands.length; i++) {
+    if (operators[i - 1] == "+") n += operands[i];
+    if (operators[i - 1] == "*") n *= operands[i];
+    if (operators[i - 1] == "|") n = Number(`${n}${operands[i]}`);
+  }
+  return n;
+}
+
+function canMakeTotal(total, operands, opPerms) {
+  for (const operators of opPerms) {
+    if (calc(operands, operators) === total) return true;
+  }
+  return false;
 }
 
 function partOne(input) {
-  // total possible permutations = (# of operands - 1) ^ 2
+  return input.reduce((count, [total, operands]) => {
+    const opPerms = permutations(operands.length - 1, ["*", "+"]);
+    if (canMakeTotal(total, operands, opPerms)) return count + total;
+    return count;
+  }, 0);
 }
 
 function partTwo(input) {
+  return input.reduce((count, [total, operands]) => {
+    const opPerms = permutations(operands.length - 1, ["*", "+", "|"]);
+    if (canMakeTotal(total, operands, opPerms)) return count + total;
+    return count;
+  }, 0);
 }
 
 function soln(rawInput) {
-  const input = rawInput.split('\n')
+  const input = rawInput
+    .split("\n")
     .map((ln) => ln.split(": "))
-    .map(([rt, ro]) => ([Number(rt), ro.split(" ").map(Number)]));
-  fmtAnsWithRuntime(() => partOne(input), () => partTwo(input));
+    .map(([rt, ro]) => [Number(rt), ro.split(" ").map(Number)]);
+  fmtAnsWithRuntime(
+    () => partOne(input),
+    () => partTwo(input)
+  );
 }
 
 module.exports = { soln };
@@ -78,4 +120,27 @@ the sum of the test values for the three equations listed above is 3749.
 
 Determine which equations could possibly be true. What is their total calibration 
 result?
+
+--- Part Two ---
+The engineers seem concerned; the total calibration result you gave them is nowhere 
+close to being within safety tolerances. Just then, you spot your mistake: some 
+well-hidden elephants are holding a third type of operator.
+
+The concatenation operator (||) combines the digits from its left and right inputs 
+into a single number. For example, 12 || 345 would become 12345. All operators are 
+still evaluated left-to-right.
+
+Now, apart from the three equations that could be made true using only addition 
+and multiplication, the above example has three more equations that can be made 
+true by inserting operators:
+
+156: 15 6 can be made true through a single concatenation: 15 || 6 = 156.
+7290: 6 8 6 15 can be made true using 6 * 8 || 6 * 15.
+192: 17 8 14 can be made true using 17 || 8 + 14.
+Adding up all six test values (the three that could be made before using only + 
+and * plus the new three that can now be made by also using ||) produces the new 
+total calibration result of 11387.
+
+Using your new knowledge of elephant hiding spots, determine which equations could 
+possibly be true. What is their total calibration result?
 */
