88fc1b87f4c354a3ecabcafd0aa1d56e128d88f7
Jonathon Alder <jonathon.alder@gmail.com>
2024-12-10 09:04:19 -0500

    2024.10.js setup

--- js/util.js
+++ js/util.js
@@ -1,7 +1,7 @@
 /**
  * Find the coordinates of a value in a 2d array. Returns undefined
  * if not found.
- * 
+ *
  * @param {any} val value to check for
  * @param {any[][]} matrix 2 dimensional array to check
  * @returns {number[] | undefined} [x, y] coordinates of value
@@ -16,8 +16,29 @@ function coordsOf(val, matrix) {
 }
 
 /**
+ * Find the coordinates of all occurances of a value in a 2d array. Returns
+ * empty array if not found.
+ *
+ * @param {any} val value to check for
+ * @param {any[][]} matrix 2 dimensional array to check
+ * @returns {[[number, number]] | undefined} [x, y] all coordinates where value exists
+ */
+function allCoordsOf(val, matrix) {
+  let x = 0;
+  const result = [];
+  for (let x = 0; x < matrix.length; x++) {
+    for (let y = 0; y < matrix[x].length; y++) {
+      if (matrix[x][y] === val) {
+        result.push([x, y]);
+      }
+    }
+  }
+  return result;
+}
+
+/**
  * Execute a function and record its runtime.
- * 
+ *
  * @param {*} f The function to execute
  * @returns {Array<string, number>} A tuple with the function result and the runtime
  */
@@ -28,41 +49,22 @@ function withRuntime(f) {
 
 /**
  * Format the output of two callbacks with their runtimes
- * 
- * @param {function()} partOne 
- * @param {function()} partTwo 
+ *
+ * @param {function()} partOne
+ * @param {function()} partTwo
  */
 function fmtAnsWithRuntime(partOne, partTwo) {
-  const [ansOne, rtOne] = withRuntime(partOne)
+  const [ansOne, rtOne] = withRuntime(partOne);
   console.log(`
-  part one: ${ansOne ?? 'incomplete'}${ansOne ? `\n  runtime (ms): ${rtOne.toFixed(4)}` : ''}`);
+  part one: ${ansOne ?? "incomplete"}${
+    ansOne ? `\n  runtime (ms): ${rtOne.toFixed(4)}` : ""
+  }`);
   const [ansTwo, rtTwo] = withRuntime(partTwo);
   console.log(`
-  part two: ${ansTwo ?? 'incomplete'}${ansTwo ? `\n  runtime (ms): ${rtTwo.toFixed(4)}` : ''}`);
+  part two: ${ansTwo ?? "incomplete"}${
+    ansTwo ? `\n  runtime (ms): ${rtTwo.toFixed(4)}` : ""
+  }`);
 }
-
-/**
- * Return all neighbors of a given coordinate in a matrix, along with their coordinates within
- * the matrix.
- * 
- * @param {Number} x index of outer array
- * @param {Number} y index of inner array
- * @param {any[][]} matrix 2d array
- * @returns {Object.<string, [any, number, number] | undefined>} An object whose keys are cardinal 
- * directions surrounding element at x, y and values are triples containing the element at that 
- * direction, its x value, and its y value
- */
-const getNeighborsWithCoordinates = (x, y, matrix) =>
-  ({
-    nw: matrix[x - 1] && [matrix[x - 1][y - 1], x - 1, y - 1],
-    n: matrix[x - 1] && [matrix[x - 1][y], x - 1, y],
-    ne: matrix[x - 1] && [matrix[x - 1][y + 1], x - 1, y + 1],
-    w: matrix[x] && [matrix[x][y - 1], x, y - 1],
-    e: matrix[x] && [matrix[x][y + 1], x, y + 1],
-    sw: matrix[x + 1] && [matrix[x + 1][y - 1], x + 1, y - 1],
-    s: matrix[x + 1] && [matrix[x + 1][y], x + 1, y],
-    se: matrix[x + 1] && [matrix[x + 1][y + 1], x + 1, y + 1],
-  });
 
 const defaultDirections = {
   nw: [-1, -1],
@@ -72,39 +74,61 @@ const defaultDirections = {
   w: [0, 1],
   sw: [1, -1],
   s: [1, 0],
-  se: [1, 1]
+  se: [1, 1],
 };
+
+const getNeighborsWithCoordinates = (
+  [x, y],
+  matrix,
+  directions = defaultDirections
+) =>
+  Object.fromEntries(
+    Object.entries(directions).map(([k, dir]) => {
+      const value = matrix[x + dir[0]]?.[y + dir[1]];
+      return [
+        k,
+        value
+          ? {
+              value,
+              coords: [x + dir[0], y + dir[1]],
+            }
+          : undefined,
+      ];
+    })
+  );
 
 /**
  * Get all neighbors of a specific element in a matrix at coordinates x and y.
- * 
+ *
  * @param {Number} x index of outer array
  * @param {Number} y index of inner array
  * @param {Object<string, [Number, Number]> | undefined} directions A map of direction names to how
  * far away they are on the coordinate axis
-*/
+ */
 const getNeighbors = (x, y, matrix, directions = defaultDirections) =>
   Object.fromEntries(
-    Object.entries(directions).map(([k, dir]) => (
-      [k, matrix[x + dir[0]]?.[y + dir[1]]]
-    )));
+    Object.entries(directions).map(([k, dir]) => [
+      k,
+      matrix[x + dir[0]]?.[y + dir[1]],
+    ])
+  );
 
-
-const isPalindrome = (str) =>
-  str === str.split('').reverse().join('');
+const isPalindrome = (str) => str === str.split("").reverse().join("");
 
 /**
  * Rotate a 2d array 90 degrees anti-clockwise
- * 
- * @param {any[][]} matrix 
+ *
+ * @param {any[][]} matrix
  * @returns {any[][]}
  */
 function rotateMatrixAntiClockwise(matrix) {
   const len = matrix[0].length;
   const out = [];
-  for (let i = 0; i < len; i++) { // 0 - 2
+  for (let i = 0; i < len; i++) {
+    // 0 - 2
     out[i] = [];
-    for (let j = 0; j < matrix.length; j++) { // 0 - 1
+    for (let j = 0; j < matrix.length; j++) {
+      // 0 - 1
       out[i][j] = matrix[j][len - i - 1];
     }
   }
@@ -113,8 +137,8 @@ function rotateMatrixAntiClockwise(matrix) {
 
 /**
  * Rotate a 2d array 90 degrees clockwise
- * 
- * @param {any[][]} matrix 
+ *
+ * @param {any[][]} matrix
  * @returns {any[][]}
  */
 function rotateMatrixClockwise(matrix) {
@@ -131,9 +155,9 @@ function rotateMatrixClockwise(matrix) {
 
 const sum = (arr) => arr.reduce((s, n) => s + n, 0);
 
-/** 
+/**
  * Greatest common denominator of a and b
- * 
+ *
  * @param {number} a
  * @param {number} b
  */
@@ -144,9 +168,9 @@ function gcd(a, b) {
   return a;
 }
 
-/** 
+/**
  * Least common multiple of a and b
- * 
+ *
  * @param {number} a
  * @param {number} b
  */
@@ -155,30 +179,30 @@ function lcm(a, b) {
 }
 
 /**
- * A map of elements in an array to the number of times they 
+ * A map of elements in an array to the number of times they
  * occur within it.
- * 
- * @param {Array<number | string>} arr 
+ *
+ * @param {Array<number | string>} arr
  */
 function occurrances(arr) {
   return arr.reduce((acc, elem) => {
     if (acc[elem] != null) {
-      acc[elem] = acc[elem] + 1
+      acc[elem] = acc[elem] + 1;
     } else {
-      acc[elem] = 1
+      acc[elem] = 1;
     }
     return acc;
-  }, {})
+  }, {});
 }
 
 /**
  * Determine whether a number is between two other numbers,
  * non-inclusive.
- * 
+ *
  * e.g. between(5, 0, 5) = false
  * e.g. between(1, 1, 3) = false
  * e.g. between(1, 0, 3) = true
- * 
+ *
  * @param {number} n The number to check
  * @param {number} a Non-inclusive start of range
  * @param {number} b Non-inclusive end of range
@@ -186,16 +210,17 @@ function occurrances(arr) {
 const between = (n, a, b) => n > a && n < b;
 
 module.exports = {
+  allCoordsOf,
+  between,
   coordsOf,
   fmtAnsWithRuntime,
-  getNeighborsWithCoordinates,
+  gcd,
   getNeighbors,
+  getNeighborsWithCoordinates,
   isPalindrome,
+  lcm,
+  occurrances,
   rotateMatrixAntiClockwise,
   rotateMatrixClockwise,
   sum,
-  gcd,
-  lcm,
-  occurrances,
-  between
 };
