1038c8eb5049fcea772ed3324db16586ffe62f54
Jonathon Alder <jonathon@fringe.us>
2024-12-10 10:51:57 -0500

    2024.10.2.js complete

--- js/y2024/d10.js
+++ js/y2024/d10.js
@@ -14,12 +14,31 @@ function findUniquePositions([x, y], grid, uniquePositions) {
   });
   const currElev = grid[x][y];
   if (currElev == 9) {
-    if (!uniquePositions.has(`${[x,y]}`)) uniquePositions.add(`${[x,y]}`);
+    if (!uniquePositions.has(`${[x, y]}`)) uniquePositions.add(`${[x, y]}`);
     return;
   }
   Object.entries(neighbors)
     .filter(([_, info]) => info != null && info.value - currElev === 1)
-    .forEach(([_, info]) => findUniquePositions(info.coords, grid, uniquePositions));
+    .forEach(([_, info]) =>
+      findUniquePositions(info.coords, grid, uniquePositions)
+    );
+}
+
+function rateTrailhead([x, y], grid) {
+  if (grid[x]?.[y] == null) return;
+  const neighbors = getNeighborsWithCoordinates([x, y], grid, {
+    n: [-1, 0],
+    e: [0, -1],
+    w: [0, 1],
+    s: [1, 0],
+  });
+  const currElev = grid[x][y];
+  if (currElev == 9) {
+    return 1;
+  }
+  return Object.entries(neighbors)
+    .filter(([_, info]) => info != null && info.value - currElev === 1)
+    .reduce((acc, [_, info]) => acc + rateTrailhead(info.coords, grid), 0);
 }
 
 function partOne(grid) {
@@ -33,7 +52,14 @@ function partOne(grid) {
   return total;
 }
 
-function partTwo(input) {}
+function partTwo(grid) {
+  const trailheads = allCoordsOf(0, grid);
+  let total = 0;
+  for (th of trailheads) {
+    total += rateTrailhead(th, grid);
+  }
+  return total;
+}
 
 function soln(rawInput) {
   const input = rawInput.split("\n").map((ln) => ln.split("").map(Number));
