8eecf10e4274a13000e3a178b1a0c2113c696787
Jonathon Alder <jonathon@fringe.us>
2024-12-11 13:02:10 -0500

    2015.5.js regex

--- js/y2015/d05.js
+++ js/y2015/d05.js
@@ -1,46 +1,47 @@
-const { fmtAnsWithRuntime } = require('../util.js');
+const { fmtAnsWithRuntime } = require("../util.js");
+
+function partOne(input) {
+  return input.reduce((total, word) => {
+    if (
+      [...word.matchAll(/[aeiou]/g)].length > 2 &&
+      word.split("").some((it, idx) => it === word[idx + 1]) &&
+      [...word.matchAll(/(ab)|(cd)|(pq)|(xy)/g)].length === 0
+    ) {
+      return total + 1;
+    } else {
+      return total;
+    }
+  }, 0);
+}
 
 function isNice(word) {
   const letters = word.split("");
-  return letters.filter(it => it === "a" || it === "e" || it === "i" || it === "o" || it === "u").length > 2
-  && letters.filter((it, idx, arr) => it === arr[idx+1]).length > 0
-  && !word.includes("ab")
-  && !word.includes("cd")
-  && !word.includes("pq")
-  && !word.includes("xy")
+  return (
+    letters.some((letter, idx, arr) => letter === arr[idx + 2]) &&
+    letters.some((letter, idx, arr) => {
+      if (arr[idx + 1] != undefined) {
+        return (
+          [...word.matchAll(new RegExp(letter + arr[idx + 1], "g"))].length > 1
+        );
+      }
+      return false;
+    })
+  );
 }
 
-function partOne(input) {
+function partTwo(input) {
   return input.reduce((total, word) => {
     if (isNice(word)) return total + 1;
     else return total;
   }, 0);
 }
 
-/**
- * @param {string} word 
- */
-function isNice2(word) {
-  const letters = word.split("");
-  return letters.some((letter, idx, arr) => letter === arr[idx+2])
-  && letters.some((letter, idx, arr) => {
-    if (arr[idx+1] != undefined) {
-      return [...word.matchAll(new RegExp(letter + arr[idx+1], "g"))].length > 1
-    }
-    return false;
-  });
-}
-
-function partTwo(input) {
-  return input.reduce((total, word) => {
-    if (isNice2(word)) return total + 1;
-    else return total;
-  }, 0);
-}
-
 function soln(rawInput) {
-  const input = rawInput.split('\n');
-  fmtAnsWithRuntime(() => partOne(input), () => partTwo(input));
+  const input = rawInput.split("\n");
+  fmtAnsWithRuntime(
+    () => partOne(input),
+    () => partTwo(input)
+  );
 }
 
 module.exports = { soln };
