347831750a19ffd821bb95c6ddf45be1cf3077cc
Jonathon Alder <jonathon@fringe.us>
2024-12-13 07:01:31 -0500

    add comment

--- js/y2024/d13.js
+++ js/y2024/d13.js
@@ -1,12 +1,20 @@
 const { fmtAnsWithRuntime } = require("../util.js");
 
+/**
+ * Use Cramer's Rule to determine the correct number of A and B button presses
+ * to reach the prize.
+ * 
+ * @param {{a: [number, number], b: [number, number], p: [number, number]}} machine 
+ * @param {number?} offset 
+ * @returns 
+ */
 function solve(machine, offset = 0) {
-  const prizes = machine.p.map((prize) => prize + offset);
+  const prize = machine.p.map((p) => p + offset);
   const determinant = machine.a[0] * machine.b[1] - machine.a[1] * machine.b[0];
   const aPresses =
-    (prizes[0] * machine.b[1] - prizes[1] * machine.b[0]) / determinant;
+    (prize[0] * machine.b[1] - prize[1] * machine.b[0]) / determinant;
   const bPresses =
-    (prizes[1] * machine.a[0] - prizes[0] * machine.a[1]) / determinant;
+    (prize[1] * machine.a[0] - prize[0] * machine.a[1]) / determinant;
   if (aPresses % 1 != 0 || bPresses % 1 != 0) {
     return 0
   }
