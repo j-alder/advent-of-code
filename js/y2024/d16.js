df90a5961537e155a27eef7cae5643bdd8c7d12c
Jonathon Alder <jonathon@fringe.us>
2024-12-17 19:33:36 -0500

    2024.16.2.js wip

--- js/y2024/d16.js
+++ js/y2024/d16.js
@@ -1,66 +1,96 @@
-const {
-  fmtAnsWithRuntime,
-  coordsOf,
-  getNeighborsWithCoordinates,
-  allCoordsOf,
-  getAllNeighborsWithCoordinates,
-} = require("../util.js");
+const { fmtAnsWithRuntime, coordsOf } = require("../util.js");
 
 const dirs = [
   [0, 1], // e
   [1, 0], // s
   [0, -1], // w
-  [-1, 0] // n
-]
+  [-1, 0], // n
+];
 
-const fmtKey = ([x, y], dir) => `${x},${y},${dir}`;
+const arrow = {
+  0: ">",
+  1: "v",
+  2: "<",
+  3: "^",
+};
 
 function getLowestScore(matrix, [sx, sy], [ex, ey]) {
-  let score = 0;
-
-  const queue = [[sx, sy, 2, 0]]; // queue of unvisited nodes
+  const queue = [[sx, sy, 0, 0]];
   const visited = new Set();
 
   while (queue.length) {
-    queue.sort((a, b) => a[3] - b[3]); // sort by closest (smallest score)
+    queue.sort((a, b) => a[3] - b[3]);
 
-    const [x, y, dir, score] = queue.shift(); // get the closest node
-    const key = fmtKey([x, y], dir); // unique identifier with node and direction
+    const [x, y, dir, score] = queue.shift();
+    const key = `${x},${y},${dir}`;
 
-    if (x === ex && y === ey) return score; // if the node is the end node, return the score -- exits early!
-    if (visited.has(key)) continue; // if already visited, skip
+    if (x === ex && y === ey) {
+      return score;
+    }
 
-    visited.add(key); // set visited
+    if (visited.has(key)) {
+      continue;
+    }
+
+    visited.add(key);
 
     const [fx, fy] = [x + dirs[dir][0], y + dirs[dir][1]]; // forward direction
-    const left = (dir + 3) % 4;
-    const right = (dir + 1) % 4;
 
     if (matrix[fx]?.[fy] !== "#") {
-      queue.push([nx, ny, dir, score + 1]); // push forward direction into queue
+      queue.push([fx, fy, dir, score + 1]); // push forward direction into queue
     }
 
-    // push all left/right dirs 
-    if (matrix[x + dirs[right][0]][y + dirs[right]] !== "#") {
-      queue.push([x, y, right, score + 1000]);
-    }
-    if (matrix[x + dirs[left][0]][y + dirs[left]] !== "#") {
-      queue.push([x, y, left, score + 1000]);
-    }
+    queue.push([x, y, (dir + 1) % 4, score + 1000]);
+    queue.push([x, y, (dir + 3) % 4, score + 1000]);
   }
-
-  return score;
-};
-
-function partOne(matrix) {
-  // 104504 too high
-  // 103504 too high
-  const start = coordsOf("S", matrix);
-  const end = coordsOf("E", matrix);
-  return getScore(matrix, start, end);
+  return -1;
 }
 
-function partTwo(matrix) {}
+function partOne(matrix) {
+  const start = coordsOf("S", matrix);
+  const end = coordsOf("E", matrix);
+  return getLowestScore(matrix, start, end);
+}
+
+function getAllPathsWithScore(matrix, [sx, sy], [ex, ey], lowestScore) {
+  const queue = [[sx, sy, 0, 0]];
+  const visited = new Set();
+
+  while (queue.length) {
+    queue.sort((a, b) => a[3] - b[3]);
+
+    const [x, y, dir, score] = queue.shift();
+    const key = `${x},${y},${dir}`;
+
+    if (x === ex && y === ey) {
+      return score;
+    }
+
+    if (visited.has(key) || score > lowestScore) {
+      continue;
+    }
+
+    visited.add(key);
+
+    const [fx, fy] = [x + dirs[dir][0], y + dirs[dir][1]]; // forward direction
+
+    if (matrix[fx]?.[fy] !== "#") {
+      queue.push([fx, fy, dir, score + 1]); // push forward direction into queue
+    }
+
+    queue.push([x, y, (dir + 1) % 4, score + 1000]);
+    queue.push([x, y, (dir + 3) % 4, score + 1000]);
+  }
+  return -1;
+
+}
+
+function partTwo(matrix) {
+  const start = coordsOf("S", matrix);
+  const end = coordsOf("E", matrix);
+  const lowestScore = getLowestScore(matrix, start, end);
+
+}
 
 function soln(rawInput) {
   const matrix = rawInput.split("\n").map((ln) => ln.split(""));
@@ -167,4 +197,61 @@ move, rotating the Reindeer from facing East to facing North.
 
 Analyze your map carefully. What is the lowest score a Reindeer could possibly 
 get?
+
+--- Part Two ---
+Now that you know what the best paths look like, you can figure out the best spot 
+to sit.
+
+Every non-wall tile (S, ., or E) is equipped with places to sit along the edges 
+of the tile. While determining which of these tiles would be the best spot to sit 
+depends on a whole bunch of factors (how comfortable the seats are, how far away 
+the bathrooms are, whether there's a pillar blocking your view, etc.), the most 
+important factor is whether the tile is on one of the best paths through the 
+maze. If you sit somewhere else, you'd miss all the action!
+
+So, you'll need to determine which tiles are part of any best path through the 
+maze, including the S and E tiles.
+
+In the first example, there are 45 tiles (marked O) that are part of at least 
+one of the various best paths through the maze:
+
+###############
+#.......#....O#
+#.#.###.#.###O#
+#.....#.#...#O#
+#.###.#####.#O#
+#.#.#.......#O#
+#.#.#####.###O#
+#..OOOOOOOOO#O#
+###O#O#####O#O#
+#OOO#O....#O#O#
+#O#O#O###.#O#O#
+#OOOOO#...#O#O#
+#O###.#.#.#O#O#
+#O..#.....#OOO#
+###############
+
+In the second example, there are 64 tiles that are part of at least one of the 
+best paths:
+
+#################
+#...#...#...#..O#
+#.#.#.#.#.#.#.#O#
+#.#.#.#...#...#O#
+#.#.#.#.###.#.#O#
+#OOO#.#.#.....#O#
+#O#O#.#.#.#####O#
+#O#O..#.#.#OOOOO#
+#O#O#####.#O###O#
+#O#O#..OOOOO#OOO#
+#O#O###O#####O###
+#O#O#OOO#..OOO#.#
+#O#O#O#####O###.#
+#O#O#OOOOOOO..#.#
+#O#O#O#########.#
+#O#OOO..........#
+#################
+
+Analyze your map further. How many tiles are part of at least one of the best 
+paths through the maze?
 */
