948c781c2993d6c0c0e89a404daeb52daa460f2f
Jonathon Alder <jonathon.alder@gmail.com>
2024-12-22 08:50:59 -0500

    2024.22.2.js complete

--- js/y2024/d22.js
+++ js/y2024/d22.js
@@ -24,7 +24,55 @@ function partOne(secretNumbers) {
   return total;
 }
 
-function partTwo(input) {
+const onesDigit = (secret) => {
+  const str = secret.toString();
+  return Number(str.charAt(str.length-1));
+}
+
+function shift(lastFive, ones) {
+  if (lastFive.length > 4) {
+    lastFive.shift();
+  }
+  lastFive.push(ones);
+}
+
+function diffs(lastFive) {
+  const result = [];
+  for (let i = 0; i < lastFive.length-1; i++) {
+    result.push(lastFive[i+1] - lastFive[i]);
+  }
+  return result;
+}
+
+function partTwo(secretNumbers) {
+  const x = {};
+  for (const initial of secretNumbers) {
+    let result = initial;
+    const lastFive = [];
+    for (let i = 0; i < 2000; i++) {
+      result = createSecretNumber(result);
+      const ones = onesDigit(result);
+      shift(lastFive, ones);
+      const diff = diffs(lastFive);
+      if (diff.length == 4) {
+        const seq = diffs(lastFive).join(",");
+        if (x[seq] == null) {
+          x[seq] = {
+            [initial]: ones
+          };
+        }
+        else if (x[seq]?.[initial] == null) {
+          x[seq][initial] = ones;
+        }
+      }
+    }
+  }
+  let maxTotal = 0;
+  for (const occs of Object.values(x)) {
+    const total = Object.values(occs).reduce((acc, ones) => acc + ones, 0);
+    if (total > maxTotal) maxTotal = total;
+  }
+  return maxTotal;
 }
 
 function soln(rawInput) {
