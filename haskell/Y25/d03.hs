6ecf9c718249ef009e495f8f2f51d6c4a1a21e9a
Jonathon Alder <jonathon.alder@gmail.com>
2025-12-27 17:24:20 -0500

    2025 day 3 p1,2 hs

--- haskell/Y25/d03.hs
+++ haskell/Y25/d03.hs
@@ -1,39 +1,36 @@
 module Y25.D03 where
 
 
-maxJoltage :: [Char] -> (Maybe Char, Maybe Char) -> Int
-maxJoltage [] (Just t, Just o) = read [t, o]
-maxJoltage (x : xs) (Nothing, Nothing) = maxJoltage xs (Just x, Nothing)
-maxJoltage (x : xs) (Just t, Nothing) = maxJoltage xs (Just t, Just x)
-maxJoltage (x : xs) (Just t, Just o) 
-  | x > t && not (null xs) = maxJoltage xs (Just x, Nothing)
-  | x > o = maxJoltage xs (Just t, Just x)
-  | otherwise = maxJoltage xs (Just t, Just o)
+import Data.Char
+import Data.List
+import Data.Maybe
 
-joltageT :: [Char] -> (Maybe Char, Maybe Char)
-joltageT = foldr (\c to -> 
-  case to of
-    (Nothing, Nothing) -> (Nothing, Just c)
-    (Nothing, Just o) -> (Just o, Just c)
-    (Just t, Just o) ->
-      let
-        t' = max c t
-        o' = if t' == c then max o t else max o c
-      in (Just t', Just o')
-  ) (Nothing, Nothing)
+type Bank = [Int]
 
-joltage :: [Char] -> Int
-joltage = 
-  let j = joltageT
-  in read [fst j, snd j]
+slice :: [a] -> Int -> Int -> [a]
+slice xs from to = take (to - from) (drop from xs)
 
-soln1 :: [String] -> Int
-soln1 = foldl (\t b -> t + maxJoltage b (Nothing, Nothing)) 0
+maxJoltage :: Bank -> Int -> Int -> Int -> Int
+maxJoltage bank since 0 result = result
+maxJoltage bank since left result =
+  let s = slice bank since (length bank - left + 1)
+      maxSoFar = maximum s
+      idx = fromJust $ elemIndex maxSoFar s
+  in maxJoltage bank (since + idx + 1) (left - 1) (result * 10 + maxSoFar)
+
+totalOutput :: Int -> [Bank] -> Int
+totalOutput cellsCount banks =
+  let poweredBanks = map (\bank -> maxJoltage bank 0 cellsCount 0) banks
+   in sum poweredBanks
 
 main :: IO ()
 main = do
-  input <- readFile "../../input/y2025/d03.txt"
-  let lns = lines input
-      s1 = soln1 lns
-  print s1
+  content <- readFile "../../input/y2025/d03.txt"
+  let
+      banks = map (map digitToInt) $ lines content
+      t1 = totalOutput 2 banks
+      t2 = totalOutput 12 banks
+   in do
+        print t1
+        print t2
 
